266b820920625694f3a85efc769d89e7b6ab3732
ASOST\2416043 <y.yuuto68@gmail.com>
2026-04-15 16:33:16 +0900

    スペース遷移とデバッグ用描画

--- AGS_2026_summer/Src/Manager/Game/SceneManager.cpp
+++ AGS_2026_summer/Src/Manager/Game/SceneManager.cpp
@@ -69,7 +69,7 @@ void SceneManager::Init(void)
 	preTime_ = std::chrono::system_clock::now();
 
 	// ‰ŠúƒV[ƒ“‚Ìİ’è
-	ChangeScene(SCENE_ID::GAME);
+	ChangeScene(SCENE_ID::TITLE);
 
 	mainScreen_ = MakeScreen(Application::SCREEN_SIZE_X, Application::SCREEN_SIZE_Y, true);
 }
--- AGS_2026_summer/Src/Scene/TitleScene.cpp
+++ AGS_2026_summer/Src/Scene/TitleScene.cpp
@@ -1,6 +1,6 @@
 #include "TitleScene.h"
 #include"../Manager/Game/SceneManager.h"
-
+#include"../Manager/Generic/KeyManager.h"
 TitleScene::TitleScene(void)
 {
 }
@@ -19,11 +19,16 @@ void TitleScene::Init(void)
 
 void TitleScene::Update(void)
 {
-	SceneManager::GetInstance().ChangeScene(SCENE_ID::GAME);
+	if (KEY::GetIns().GetInfo(KEY::KEY_TYPE::ENTER).down)
+	{
+		SceneManager::GetInstance().ChangeScene(SCENE_ID::GAME);
+	}
 }
 
 void TitleScene::Draw(void)
 {
+	DrawString(0, 0, "title", 0xffffff);
+
 }
 
 void TitleScene::Release(void)
